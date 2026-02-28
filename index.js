51cf86cecbde2608696ea88ec62dbff4410b9a6a
pavithrapn49 <pavithrapn49@gmail.com>
2026-02-28 11:25:21 +0530

    CHANGES done

--- index.js
+++ index.js
@@ -14,11 +14,13 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
-
+app.get("/", (req, res) => {
+  res.send("JWT Authentication API is running 🚀");
+});
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.error("MongoDB Error:", err));
-  
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
